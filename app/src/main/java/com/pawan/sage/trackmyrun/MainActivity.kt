8b3caad255820cc49966d45809a2e2d267a6e076
pawan dhami <pawan.dhami3451@gmail.com>
2021-12-05 22:30:49 +1100

    bottom navigation progressed

--- app/src/main/java/com/pawan/sage/trackmyrun/MainActivity.kt
+++ app/src/main/java/com/pawan/sage/trackmyrun/MainActivity.kt
@@ -2,13 +2,38 @@ package com.pawan.sage.trackmyrun
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.pawan.sage.trackmyrun.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        val navView: BottomNavigationView = binding.bottomNavigationView
+
+        val navController = findNavController(R.id.navHostFragment)
+
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.runFragment, R.id.statsFragment, R.id.settingsFragment
+            )
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
  
     }
 }
