9a83e1c1219b11060d47b1c2e951ce0007ebaa96
Pawan <pawan.dhami3451@gmail.com>
2021-12-13 09:39:59 +1100

    Mapview setup in tracking fragment

--- app/src/main/java/com/pawan/sage/trackmyrun/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/pawan/sage/trackmyrun/ui/fragments/TrackingFragment.kt
@@ -1,14 +1,74 @@
 package com.pawan.sage.trackmyrun.ui.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.google.android.gms.maps.GoogleMap
 import com.pawan.sage.trackmyrun.R
+import com.pawan.sage.trackmyrun.databinding.FragmentTrackingBinding
 import com.pawan.sage.trackmyrun.ui.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class TrackingFragment : Fragment(R.layout.fragment_tracking) {
+class TrackingFragment : Fragment() {
 
     //to inject viewModel from dagger here
     private val viewModel: MainViewModel by viewModels()
+
+    private lateinit var binding: FragmentTrackingBinding
+
+    private var map: GoogleMap ?= null //base map object
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = FragmentTrackingBinding.inflate(inflater, container, false)
+
+        binding.mapView.onCreate(savedInstanceState)
+
+        binding.mapView.getMapAsync{
+            map = it
+        }
+
+        return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.mapView?.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.mapView?.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding.mapView?.onStop()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.mapView?.onPause()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.mapView?.onLowMemory()
+    }
+
+    //to cache the map
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        binding.mapView?.onSaveInstanceState(outState)
+    }
+
 }
