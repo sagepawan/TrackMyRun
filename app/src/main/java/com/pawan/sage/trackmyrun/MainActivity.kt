03f7f8e70536e9eb6e040860d1b2568c4491d99d
Pawan <pawan.dhami3451@gmail.com>
2021-12-14 22:41:15 +1100

    foreground service details added

--- app/src/main/java/com/pawan/sage/trackmyrun/MainActivity.kt
+++ app/src/main/java/com/pawan/sage/trackmyrun/MainActivity.kt
@@ -1,27 +1,33 @@
 package com.pawan.sage.trackmyrun
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.pawan.sage.trackmyrun.databinding.ActivityMainBinding
+import com.pawan.sage.trackmyrun.otherpackages.Constants.ACTION_SHOW_TRACKING_FRAGMENT
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        navigateToTrackingFragmentWhenRequired(intent)
 
         //setSupportActionBar(binding.toolbar)
 
@@ -39,7 +45,7 @@ class MainActivity : AppCompatActivity() {
 
         val navView: BottomNavigationView = binding.bottomNavigationView
 
-        val navController = findNavController(R.id.navHostFragment)
+        navController = findNavController(R.id.navHostFragment)
 
         navView.setupWithNavController(navController)
 
@@ -47,11 +53,11 @@ class MainActivity : AppCompatActivity() {
 
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        /*val appBarConfiguration = AppBarConfiguration(
+        val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.runFragment, R.id.statsFragment, R.id.settingsFragment
             )
-        )*/
+        )
 
         /*navController.addOnDestinationChangedListener{ _, destination, _ ->
             when(destination.id){
@@ -65,6 +71,19 @@ class MainActivity : AppCompatActivity() {
         //setupActionBarWithNavController(navController, appBarConfiguration)
         //navView.setupWithNavController(navController)
 
+    }
+
+    //if activity wasn't destroyed and reused the pending intent to launch
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        navigateToTrackingFragmentWhenRequired(intent)
+    }
+
+    //take user to tracking fragment once MainActivity is relaunched from clicking notification
+    private fun navigateToTrackingFragmentWhenRequired(intent: Intent?){
+        if(intent?.action == ACTION_SHOW_TRACKING_FRAGMENT) {
+            navController.navigate(R.id.action_global_trackingFragment)
+        }
     }
 
 }
--- app/src/main/java/com/pawan/sage/trackmyrun/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/pawan/sage/trackmyrun/ui/fragments/TrackingFragment.kt
@@ -1,8 +1,17 @@
 package com.pawan.sage.trackmyrun.ui.fragments
 
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.google.android.gms.maps.GoogleMap
 import com.pawan.sage.trackmyrun.R
+import com.pawan.sage.trackmyrun.databinding.FragmentTrackingBinding
+import com.pawan.sage.trackmyrun.otherpackages.Constants.ACTION_START_RESUME_SERVICE
+import com.pawan.sage.trackmyrun.service.TrackingService
 import com.pawan.sage.trackmyrun.ui.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -30,8 +39,19 @@ class TrackingFragment : Fragment() {
             map = it
         }
 
+        binding.btnToggleRun.setOnClickListener{
+            sendCommandToService(ACTION_START_RESUME_SERVICE)
+        }
+
         return binding.root
     }
+
+    //to communicate with service
+    private fun sendCommandToService(action: String) =
+        Intent(requireContext(), TrackingService::class.java).also {
+            it.action = action
+            requireContext().startService(it)
+        }
 
     override fun onResume() {
         super.onResume()
