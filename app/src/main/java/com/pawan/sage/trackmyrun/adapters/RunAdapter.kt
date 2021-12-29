2cf0dab75d148a9becd256944a35b254e3253771
Pawan <pawan.dhami3451@gmail.com>
2021-12-29 23:58:42 +1100

    setup for custom adapter to show runs added\

---
+++ app/src/main/java/com/pawan/sage/trackmyrun/adapters/RunAdapter.kt
@@ -0,0 +1,22 @@
+package com.pawan.sage.trackmyrun.adapters
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class RunAdapter: RecyclerView.Adapter<RunAdapter.RunViewHolder>() {
+
+    inner class RunViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RunViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RunViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
