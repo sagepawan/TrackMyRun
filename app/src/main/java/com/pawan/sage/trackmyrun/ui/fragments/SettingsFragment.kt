bb0b858730bc7c5bdd1cc2dd3d044968ba73daee
Pawan <pawan.dhami3451@gmail.com>
2022-01-02 10:32:52 +1100

    complete changes for settingFragment to allow user to change username and weight from settings page

--- app/src/main/java/com/pawan/sage/trackmyrun/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/pawan/sage/trackmyrun/ui/fragments/SettingsFragment.kt
@@ -1,9 +1,77 @@
 package com.pawan.sage.trackmyrun.ui.fragments
 
 
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
 import com.pawan.sage.trackmyrun.R
+import com.pawan.sage.trackmyrun.databinding.FragmentSettingsBinding
+import com.pawan.sage.trackmyrun.otherpackages.Constants.KEY_NAME
+import com.pawan.sage.trackmyrun.otherpackages.Constants.KEY_WEIGHT
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class SettingsFragment : Fragment(R.layout.fragment_settings) {
 
+    private lateinit var binding: FragmentSettingsBinding
+
+    @Inject
+    lateinit var sharedPreferences: SharedPreferences
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnApplyChanges.setOnClickListener{
+            val success = applyChangesToSharedPrefs()
+
+            if(success){
+                Snackbar.make(view, "Changes saved!", Snackbar.LENGTH_LONG).show()
+            } else {
+                Snackbar.make(view, "Please fill out all fields", Snackbar.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSettingsBinding.inflate(inflater, container, false)
+
+
+
+        return binding.root
+    }
+
+    private fun loadChangesFromSharedPref() {
+        val name = sharedPreferences.getString(KEY_NAME, "")
+        val weight = sharedPreferences.getFloat(KEY_WEIGHT, 80f)
+        binding.etName.setText(name)
+        binding.etWeight.setText(weight.toString())
+    }
+
+    private fun applyChangesToSharedPrefs(): Boolean {
+        val nameText = binding.etName.text.toString()
+        val weightText = binding.etWeight.text.toString()
+
+        if(nameText.isEmpty()||weightText.isEmpty()){
+            return false
+        }
+
+        sharedPreferences.edit()
+            .putString(KEY_NAME, nameText)
+            .putFloat(KEY_WEIGHT, weightText.toFloat())
+            .apply()
+
+        val toolbarText = "Let's go $nameText!"
+        requireActivity().findViewById<TextView>(R.id.tvToolbarTitle).text = toolbarText
+        return true
+    }
 }
